e18148b729aca140f85c30cb1b59bec8ef9e8f95
jose pedraza <jose.pedraza@mail.udp.cl>
2016-05-19 00:42:18 -0400

    primero

--- conway.c
+++ conway.c
@@ -6,6 +6,18 @@
 #include <sys/time.h>
 #include <sys/types.h>
 
+#define red   "\033[0;31m"        /* 0 -> normal ;  31 -> red */
+#define cyan  "\033[1;36m"        /* 1 -> bold ;  36 -> cyan */
+#define green "\033[4;32m"        /* 4 -> underline ;  32 -> green */
+#define blue  "\033[9;34m"        /* 9 -> strike ;  34 -> blue */
+ 
+#define black  "\033[0;30m"
+#define brown  "\033[0;33m"
+#define magenta  "\033[0;35m"
+#define gray  "\033[0;37m"
+ 
+#define none   "\033[0m"        /* to flush the previous property */
+
 
 int dst = 0;
 int rondas = 0;
@@ -331,7 +343,8 @@ void Imprimir_tablero(PMYDATA s, int o){
     printf("\n");
     rondas = rondas + 1;
     printf("Vivos: %i. Ronda: %i.\n", c, rondas); 
-     printf ("\033[32m Salida en color: Verde\n");
+    printf("%sHello, %sworld!%s\n", red, blue, none);
+    printf("%sHello%s, %sworld!\n", green, none, cyan);
     s->z = 0;
  }
 }
